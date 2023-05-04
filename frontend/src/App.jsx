4ce6c46fef3243567c0c17b7011df7dc483938cb
Josiah <dkaramazov56@gmail.com>
2023-05-04 17:20:18 -0400

    working on getting together a nice looking dashboard and header footer etc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,5 @@
-import { AppBar, Toolbar } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Dashboard from "./components/Dashboard";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <main></main>
-      <Footer />
-    </>
-  );
+  return <Dashboard />;
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import theme from "./theme";
+import { Box } from "@mui/material";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
